7e134ec7606d2840e408f453557a8671b4d993bd
Isabelly Cristine <117411164+isahCris@users.noreply.github.com>
2022-12-02 10:29:55 -0300

    Create pares.java

---
+++ laçoFor/pares.java
@@ -0,0 +1,18 @@
+package la�os;
+
+import java.util.Scanner;
+
+public class Pares {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int i;
+		for (i=0;i<=500;i++) {
+			if (i %2 == 0) {
+			System.out.println(+i);
+			}
+		}
+
+	}
+
+}
