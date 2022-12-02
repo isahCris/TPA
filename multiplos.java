d8a0a2d57f1e5177d1140d4ef0dffdfc454ccedb
isahCris <117411164+isahCris@users.noreply.github.com>
2022-12-01 23:14:03 -0300

    laços

---
+++ multiplos.java
@@ -0,0 +1,17 @@
+package laços;
+import java.util.Scanner;
+public class multiplos {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int i;
+		for (i=1;i<=100;i++) {
+			if (i % 10 == 0) {
+			System.out.println(+i);
+			}
+		}
+
+	}
+	}
+
+
